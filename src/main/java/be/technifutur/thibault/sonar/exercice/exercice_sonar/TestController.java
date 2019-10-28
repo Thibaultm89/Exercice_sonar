21963594a8b1840f1a3240fd2635845586fb32ca
Thibault Molle <t.molle@afelio.be>
2019-10-28 15:25:31 +0100

    test

--- src/main/java/be/technifutur/thibault/sonar/exercice/exercice_sonar/TestController.java
+++ src/main/java/be/technifutur/thibault/sonar/exercice/exercice_sonar/TestController.java
@@ -1,8 +1,11 @@
 package be.technifutur.thibault.sonar.exercice.exercice_sonar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +18,14 @@ import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+//@Log4j2
 
 @RestController
 @SessionAttributes("hello")
 public class TestController {
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
+
 
     @RequestMapping(method = GET)
     private String tester() {
