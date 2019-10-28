d033bb4955e5567789acd303ee2c888dd5119abd
Thibault Molle <t.molle@afelio.be>
2019-10-28 11:20:04 +0100

    first commit

---
+++ src/main/java/be/technifutur/thibault/sonar/exercice/exercice_sonar/ExerciceSonarApplication.java
@@ -0,0 +1,13 @@
+package be.technifutur.thibault.sonar.exercice.exercice_sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciceSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciceSonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/technifutur/thibault/sonar/exercice/exercice_sonar/ExerciceSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technifutur.thibault.sonar.exercice.exercice_sonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciceSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
